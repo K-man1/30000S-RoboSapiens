9c91bb8b6d27a45229b7e1eb5557454c1f3a1584
K-man1 <karman.chhatwal@gmail.com>
2025-12-05 18:24:25 -0500

    started auton

--- src/main.cpp
+++ src/main.cpp
@@ -25,8 +25,8 @@ pros::adi::DigitalOut wing('C');
 bool wing_state = true;
 
 // motor groups
-pros::MotorGroup leftMotors({11, -12, 13}, pros::MotorGearset::blue); // left motor group - ports 3 (reversed), 4, 5 (reversed)
-pros::MotorGroup rightMotors({-14, 15, -16}, pros::MotorGearset::blue); // right motor group - ports 6, 7, 9 (reversed)
+pros::MotorGroup leftMotors({-11, 12, -13}, pros::MotorGearset::blue); // left motor group - ports 3 (reversed), 4, 5 (reversed)
+pros::MotorGroup rightMotors({14, -15, 16}, pros::MotorGearset::blue); // right motor group - ports 6, 7, 9 (reversed)
 
 // Inertial Sensor on port 20
 pros::Imu imu(17);
@@ -49,26 +49,27 @@ lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
 );
 
 // lateral motion controller
-lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+lemlib::ControllerSettings lateral_controller(6.3, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              3, // derivative gain (kD)
-                                              3, // anti windup
-                                              1, // small error range, in inches
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in inches
-                                              500, // large error range timeout, in milliseconds
-                                              20 // maximum acceleration (slew)
+                                              8, // derivative gain (kD)
+                                              0, // anti windup
+                                              0, // small error range, in inches
+                                              0, // small error range timeout, in milliseconds
+                                              0, // large error range, in inches
+                                              0, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
 );
 
+
 // angular PID controller
-lemlib::ControllerSettings angular_controller(0.5, // proportional gain (kP)
+lemlib::ControllerSettings angular_controller(1.8, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              5 , // derivative gain (kD)
+                                              12, // derivative gain (kD)
                                               3, // anti windup
-                                              1, // small error range, in degrees
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in degrees
-                                              500, // large error range timeout, in milliseconds
+                                              0, // small error range, in degrees
+                                              0, // small error range timeout, in milliseconds
+                                              0, // large error range, in degrees
+                                              0, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
 );
 
@@ -146,7 +147,11 @@ void autonomous() {
     // set position to x:0, y:0, heading:0
     chassis.setPose(0, 0, 0);
     // turn to face heading 90 with a very long timeout
-    chassis.turnToHeading(90, 100000);
+    chassis.moveToPoint(0, -17, 5000, {.forwards = false});
+    chassis.moveToPoint(12, -27.5, 5000, {.forwards = false});
+
+
+
 }
 
 /**
@@ -159,7 +164,7 @@ void opcontrol() {
     // drivetrain
     int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
     int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
-    chassis.arcade(leftY, -rightX);
+    chassis.arcade(-leftY, rightX);
 
     // Scorer control (Port 1)
     // - Turn forward when L2 is pressed
