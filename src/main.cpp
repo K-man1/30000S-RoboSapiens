a57d528c345993b02184f601d1b95de81977b78c
K-man1 <karman.chhatwal@gmail.com>
2025-12-31 19:50:50 -0500

    While intaking, hood turns back at max speed

--- src/main.cpp
+++ src/main.cpp
@@ -7,35 +7,31 @@
 #include "pros/adi.hpp"
 
 
-// controller
-pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 // user-operated motors
 // Port 1 = Scorer
-pros::Motor scorer(3);
+pros::MotorGroup scorer({-16, 17});
 // Port 2 & 3 = Intake (use MotorGroup to control both together)
-pros::MotorGroup intake({-1, 2});
+pros::Motor intake(1);
 
-pros::adi::DigitalOut double_park('H');
-bool doubleParkState = true;
-pros::adi::DigitalOut will('A');
+pros::adi::DigitalOut will('H');
 bool willState = true;
 pros::adi::DigitalOut middle_goal('B');
 
-pros::adi::DigitalOut wing('C');
-bool wing_state = true;
+// pros::adi::DigitalOut wing('C');
+// bool wing_state = true;
 
 // motor groups
-pros::MotorGroup leftMotors({-11, 12, -13}, pros::MotorGearset::blue); // left motor group - ports 3 (reversed), 4, 5 (reversed)
-pros::MotorGroup rightMotors({14, -15, 16}, pros::MotorGearset::blue); // right motor group - ports 6, 7, 9 (reversed)
+pros::MotorGroup leftMotors({8, -9, -10}, pros::MotorGearset::blue); // left motor group - ports 3 (reversed), 4, 5 (reversed)
+pros::MotorGroup rightMotors({-7,6,5}, pros::MotorGearset::blue); // right motor group - ports 6, 7, 9 (reversed)
 
 // Inertial Sensor on port 20
-pros::Imu imu(17);
+pros::Imu imu(2);
 
 // tracking wheels
 
 // vertical tracking wheel encoder. Rotation sensor, port 19, reversed
-pros::Rotation verticalEnc(10);
+pros::Rotation verticalEnc(4);
 
 // vertical tracking wheel. 2.75" diameter, 0" offset
 lemlib::TrackingWheel vertical(&verticalEnc, lemlib::Omniwheel::NEW_2, 0.25);
@@ -50,27 +46,27 @@ lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
 );
 
 // lateral motion controller
-lemlib::ControllerSettings lateral_controller(6.3, // proportional gain (kP)
+lemlib::ControllerSettings lateral_controller(6.25, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              8, // derivative gain (kD)
-                                              0, // anti windup
-                                              0, // small error range, in inches
-                                              0, // small error range timeout, in milliseconds
-                                              0, // large error range, in inches
-                                              0, // large error range timeout, in milliseconds
-                                              0 // maximum acceleration (slew)
+                                              5, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
 );
 
 
 // angular PID controller
-lemlib::ControllerSettings angular_controller(1.8, // proportional gain (kP)
+lemlib::ControllerSettings angular_controller(1.235, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              12, // derivative gain (kD)
+                                              6, // derivative gain (kD)
                                               3, // anti windup
-                                              0, // small error range, in degrees
-                                              0, // small error range timeout, in milliseconds
-                                              0, // large error range, in degrees
-                                              0, // large error range timeout, in milliseconds
+                                              1, // small error range, in degrees
+                                              500, // small error range timeout, in milliseconds
+                                              2, // large error range, in degrees
+                                              1000, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
 );
 
@@ -117,7 +113,11 @@ void initialize() {
     pros::Task screen_task([&]() {
         while (true) {
             // print robot location to the brain screen
-            
+            pros::lcd::print(0, "X: %f", chassis.getPose().x);
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y);
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta);
+            pros::lcd::print(3, "IMU: %f", (double)imu.get_rotation());
+
             // delay to save resources
             pros::delay(20);
         }
@@ -146,117 +146,106 @@ void competition_initialize() {}
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
-    // set position to x:0, y:0, heading:0
+
     chassis.setPose(0, 0, 0);
+
+    //Route to loader
     
-    
-    chassis.moveToPoint(0, -29, 1000, {.forwards = false, .maxSpeed = 80}); // forward to laoder 
-    chassis.turnToHeading(-85, 750); //turn to align to loader
-    intake.move(100);
+    //Move forward to loader
+    chassis.moveToPoint(0, 30.5, 5000);
+    //deploy little will
     will.set_value(true);
-    chassis.moveToPoint(30, -31, 1500, {.forwards = false, .maxSpeed = 70});
-    //move forward to loader and get blocks
-    chassis.moveToPoint(-25, -33.5, 2000); //from loader to goal
-    chassis.waitUntil(20);.         
+    willState = true;
+    //start intake
+    intake.move(127);
+    scorer.move(-10);
+    //Turn to face loader
+    chassis.turnToHeading(-90, 3000);
+    // Go into loader
+    chassis.moveToPoint(-15, 30.5, 5000);
+
+    //STAY HERE UNTIL OPTICAL SENSOR SEES BLUE. ADD CODE HERE.
+
+    pros::delay(1000);
+    //Loader to goal
+
+    //Back from loader into goal
+    chassis.moveToPoint(20, 32.1, 5000, {.forwards = false});
+
+    //Score blocks
     scorer.move(127);
-    pros::delay(4000);
+    pros::delay(2500);
+
+    //Route to middle goal
+
+    //leave goal
     will.set_value(false);
-    //wings time
-    chassis.moveToPose(chassis.getPose().x + 30, -45, -90, 2000);
+    willState = false;
+    chassis.turnToHeading(180, 3000);
 
-    chassis.moveToPoint(chassis.getPose().x - 30, chassis.getPose().y + 20, 2000, {.forwards = false});
+    //intake corner blocks
+    chassis.moveToPoint(24, 10, 5000);
 
-    // //push
-    // chassis.moveToPoint(chassis.getPose().x + 6, chassis.getPose().y, 500, {.forwards = false});
-    // pros::delay(500);
-    // will.set_value(true);
-    // chassis.moveToPoint(chassisddddrrrrrtghuuh`.getPose().x - 6, chassis.getPose().y, 500, {.minSpeed = 127});
-    // pros::delay(500);
-// //BTW AT THIS POINT THE ROBOT IS AT X = -8 AND Y = -30 THETA = 90
-//     chassis.moveToPoint(-5, chassis.getPose().y, 1500, {.forwards = false}); //back away from goal
-//     will.set_value(false);
-//     intake.move(100);
-//     scorer.move(-20);
-//     chassis.turnToHeading(115, 1000);
-//     chassis.moveToPoint(-30, -2, 2000, {.forwards = false, .maxSpeed = 75});
-//     chassis.moveToPose(-35, 20, 115, 1000);
-//     chassis.moveToPoint(-40, 25, 1000);
-//     // chassis.turnToHeading(-135, 4000);
-//     // chassis.moveToPoint(-40, 15, 2000);
-//     pros::delay(1000);
-//     scorer.move(80);
-//     middle_goal.set_value(true);
-
+    
 }
 
 /**
  * Runs in driver control
  */
-void opcontrol() {
-    // controller
-    // loop to continuously update motors
-    while (true) {
-    // drivetrain
-    int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-    int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
-    chassis.arcade(-leftY, rightX);
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
-    // Scorer control (Port 1)
-    // - Turn forward when L2 is pressed
-    if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+void opcontrol() {
+    // loop forever
+    while (true) {
+        // get left y and right x positions
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+
+        // move the robot
+        chassis.arcade(leftY, rightX);
+
+        // score
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
         scorer.move(127);
         intake.move(127);
-    } 
-    
-    else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
-        scorer.move(-127);
-    }
-
-    else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
-        intake.move(-127);
-    } 
-    
-    else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+        } 
+        
+        // middle goal
+        else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
+        {
+        scorer.move(64);
         intake.move(127);
-        scorer.move(-10); // stop scorer when intaking
-    }
+        middle_goal.set_value(false);
+        }
 
-    else {
-        intake.move(0);
-        scorer.move(0);
-    }
 
-    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B))
-    {
-        doubleParkState = !doubleParkState;
-        double_park.set_value(doubleParkState);
-    }
+        // outtake everythin
+        else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+            intake.move(-127);
+            scorer.move(-127);
+        } 
+        
+        // just intake
+        else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+            intake.move(127);
+            scorer.move(-127); // stop scorer when intaking
+        }
 
-    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_Y))
+        else {
+            intake.move(0);
+            scorer.move(0);
+            middle_goal.set_value(true);
+        }
+
+        if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_Y))
     {
         willState = !willState;
         will.set_value(willState);
     }
 
-    if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
-    {
-        scorer.move(64);
-        intake.move(127);
-        middle_goal.set_value(true);
+        // delay to save resources
+        pros::delay(25);
     }
-    else
-    {
-        middle_goal.set_value(false);
-    }
-
-    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_RIGHT))
-    {
-        wing_state = !wing_state;
-        wing.set_value(wing_state);
-    }
-
     
-    pros::delay(10);
-}
 
 }
